5177b5823e573649de8e008a0a30845adc5bdb66
Omer <ramic.omer82@gmail.com>
2016-07-17 17:09:05 +0200

    Update IP-Obfuscator.py

--- IP-Obfuscator.py
+++ IP-Obfuscator.py
@@ -9,9 +9,9 @@ def obscure_ip(ip):
 	print('[~] Obfuscated IPs:\n')
 	for match in re.finditer(r'((?P<a>\d+)\.)((?P<b>\d+)\.)((?P<c>\d+)\.)(?P<d>\d+)', ip):
 
-		print('[+] http://'+str(int(match.group('a'))*256**3+int(match.group('b'))*256**2+int(match.group('c'))*256**1+int(match.group('d'))))
-		print('[+] http://'+str(hex(int(match.group('a'))*256**3+int(match.group('b'))*256**2+int(match.group('c'))*256**1+int(match.group('d')))))
-		print('[+] http://'+re.sub('o','',str(oct(int(match.group('a'))*256**3+int(match.group('b'))*256**2+int(match.group('c'))*256**1+int(match.group('d')))))+'\n')
+		print('[+] http://'+str(int(match.group('a'))*256**3+int(match.group('b'))*256**2+int(match.group('c'))*256+int(match.group('d'))))
+		print('[+] http://'+str(hex(int(match.group('a'))*256**3+int(match.group('b'))*256**2+int(match.group('c'))*256+int(match.group('d')))))
+		print('[+] http://'+re.sub('o','',str(oct(int(match.group('a'))*256**3+int(match.group('b'))*256**2+int(match.group('c'))*256+int(match.group('d')))))+'\n')
 
 		print('[+] http://'+re.sub('o','',str(oct(int(match.group('a'))))+'.'+str(oct(int(match.group('b'))))+'.'+str(oct(int(match.group('c'))))+'.'+str(oct(int(match.group('d'))))))
 		print('[+] http://'+re.sub('o','0000000',str(oct(int(match.group('a'))))+'.'+str(oct(int(match.group('b'))))+'.'+str(oct(int(match.group('c'))))+'.'+str(oct(int(match.group('d'))))))
@@ -32,12 +32,12 @@ def obscure_ip(ip):
 		print('[+] http://'+re.sub('o','',match.group('a')+'.'+match.group('b')+'.'+str(oct(int(match.group('c'))))+'.'+str(oct(int(match.group('d'))))))
 		print('[+] http://'+re.sub('o','',match.group('a')+'.'+match.group('b')+'.'+match.group('c')+'.'+str(oct(int(match.group('d')))))+'\n')
 		
-		print('[+] http://'+str(hex(int(match.group('a'))))+'.'+str(hex(int(match.group('b'))))+'.'+str(int(match.group('c'))*256**1+int(match.group('d'))))
-		print('[+] http://'+str(hex(int(match.group('a'))))+'.'+str(int(match.group('b'))*256**2+int(match.group('c'))*256**1+int(match.group('d'))))
-		print('[+] http://'+re.sub('o','',str(oct(int(match.group('a'))))+'.'+str(oct(int(match.group('b')))))+'.'+str(int(match.group('c'))*256**1+int(match.group('d'))))
-		print('[+] http://'+re.sub('o','',str(oct(int(match.group('a')))))+'.'+str(int(match.group('b'))*256**2+int(match.group('c'))*256**1+int(match.group('d'))))
-		print('[+] http://'+str(hex(int(match.group('a'))))+'.'+re.sub('o','',str(oct(int(match.group('b')))))+'.'+str(int(match.group('c'))*256**1+int(match.group('d'))))
-		print('[+] http://'+re.sub('o','',str(oct(int(match.group('a')))))+'.'+str(hex(int(match.group('b'))))+'.'+str(int(match.group('c'))*256**1+int(match.group('d'))))
+		print('[+] http://'+str(hex(int(match.group('a'))))+'.'+str(hex(int(match.group('b'))))+'.'+str(int(match.group('c'))*256+int(match.group('d'))))
+		print('[+] http://'+str(hex(int(match.group('a'))))+'.'+str(int(match.group('b'))*256**2+int(match.group('c'))*256+int(match.group('d'))))
+		print('[+] http://'+re.sub('o','',str(oct(int(match.group('a'))))+'.'+str(oct(int(match.group('b')))))+'.'+str(int(match.group('c'))*256+int(match.group('d'))))
+		print('[+] http://'+re.sub('o','',str(oct(int(match.group('a')))))+'.'+str(int(match.group('b'))*256**2+int(match.group('c'))*256+int(match.group('d'))))
+		print('[+] http://'+str(hex(int(match.group('a'))))+'.'+re.sub('o','',str(oct(int(match.group('b')))))+'.'+str(int(match.group('c'))*256+int(match.group('d'))))
+		print('[+] http://'+re.sub('o','',str(oct(int(match.group('a')))))+'.'+str(hex(int(match.group('b'))))+'.'+str(int(match.group('c'))*256+int(match.group('d'))))
 		
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description=NAME+VERSION, epilog=AUTHOR)
